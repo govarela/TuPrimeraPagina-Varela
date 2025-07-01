694ea6a6e7e0a6983338ba90db2c911fd918d99c
Gonzalo Varela <gonzalovareladossantos@email.com>
2025-06-30 21:40:09 -0300

    tercera version

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('', views.index, name='index'),
-    path('agregar-artista/', views.agregar_artista, name='agregar_artista'),
-    path('agregar-album/', views.agregar_album, name='agregar_album'),
-    path('agregar-cancion/', views.agregar_cancion, name='agregar_cancion'),
-    path('buscar-cancion/', views.buscar_cancion, name='buscar_cancion'),
+    path('agregar-artista/', views.agregar_artista, name='artista_create'),
+    path('agregar-album/', views.agregar_album, name='album_create'),
+    path('agregar-cancion/', views.agregar_cancion, name='cancion_create'),
+    path('buscar/', views.buscar_cancion, name='buscar'),
 ]
