ece2ff4ce0ea36d7ce7f4aec0c0c92630e1d5942
Gonzalo Varela <gonzalovareladossantos@email.com>
2025-06-30 21:29:58 -0300

    segunda version

--- core/views.py
+++ core/views.py
@@ -1,48 +1,39 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .models import Artista, Album, Cancion
 from .forms import ArtistaForm, AlbumForm, CancionForm, BusquedaCancionForm
 
 def index(request):
-    return render(request,'core/index.html')
-
-
-def home(request):
-    return render(request, 'home.html')
-
+    return render(request, 'core/index.html')
 
 def agregar_artista(request):
     if request.method == 'POST':
         form = ArtistaForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('index')
     else:
         form = ArtistaForm()
-    return render(request, 'agregar_artista.html', {'form': form})
-
+    return render(request, 'core/agregar_artista.html', {'form': form})
 
 def agregar_album(request):
     if request.method == 'POST':
         form = AlbumForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('index')
     else:
         form = AlbumForm()
-    return render(request, 'agregar_album.html', {'form': form})
-
+    return render(request, 'core/agregar_album.html', {'form': form})
 
 def agregar_cancion(request):
     if request.method == 'POST':
         form = CancionForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('index')
     else:
         form = CancionForm()
-    return render(request, 'agregar_cancion.html', {'form': form})
-
+    return render(request, 'core/agregar_cancion.html', {'form': form})
 
 def buscar_cancion(request):
     canciones = []
@@ -53,4 +44,4 @@ def buscar_cancion(request):
             canciones = Cancion.objects.filter(titulo__icontains=titulo)
     else:
         form = BusquedaCancionForm()
-    return render(request, 'buscar_cancion.html', {'form': form, 'canciones': canciones})
+    return render(request, 'core/buscar_cancion.html', {'form': form, 'canciones': canciones})
